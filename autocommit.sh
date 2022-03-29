07c28a47bc4a16943ca0629f95bfdeca22495e3c
Weathercold <weathercold.scr@gmail.com>
2022-03-28 21:42:06 -0400

    Import 14

--- autocommit.sh
+++ autocommit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$BASH_SOURCE") $BASEDIR/dotdrop update -f
+cd $(dirname "$BASH_SOURCE")
+pwd
+./dotdrop.sh update -f
 git add .
 git commit -m "Import $(( $(git log -1 --pretty=format:%B | grep -oP '\d*') + 1 ))"
 git push
