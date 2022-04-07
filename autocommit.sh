feb01df03eed6c79b56e49b844b14d5a587a1115
Weathercold <weathercold.scr@gmail.com>
2022-04-07 18:05:50 -0400

    Autoupdate 5

--- autocommit.sh
+++ autocommit.sh
@@ -4,5 +4,5 @@ cd $(dirname "$BASH_SOURCE")
 git pull
 ./dotdrop.sh update -f
 git add .
-git commit -m "Import $(( $(git log -10 --pretty=format:%B | grep -Pom1 'Import \K\d*') + 1 ))"
+git commit -m "Autoupdate $(( $(git log -10 --pretty=format:%B | grep -Pom1 'Import \K\d*') + 1 ))"
 git push
