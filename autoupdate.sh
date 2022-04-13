fb4f33e5bb677740b0ebfad7f6d31092feb4702e
Weathercold <weathercold.scr@gmail.com>
2022-04-13 00:00:13 -0400

    Autoupdate 9

--- autoupdate.sh
+++ autoupdate.sh
@@ -2,7 +2,8 @@
 
 cd $(dirname "$BASH_SOURCE")
 git pull
-./dotdrop.sh update -f
+./dotdrop.sh update -fp user
+sudo ./dotdrop.sh update -fp system
 git add .
 git commit -m "Autoupdate $(( $(git log -10 --pretty=format:%B | grep -Pom1 'Autoupdate \K\d*') + 1 ))"
 git push
