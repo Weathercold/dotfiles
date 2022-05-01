dcce2879f6d2a690d348ef716b386fa1f17a5813
Weathercold <weathercold.scr@gmail.com>
2022-05-01 08:19:47 -0400

    Autoupdate 23

--- autoupdate.sh
+++ autoupdate.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$BASH_SOURCE")
 ./dotdrop.sh update -bfp user
 ./dotdrop.sh update -bfp system
-./dotdrop.sh update -bfp $(cat ./current_theme)
+./dotdrop.sh update -bfp $(cat current_theme)
 git add .
 git commit -m "Autoupdate $(( $(git log -10 --pretty=format:%B | grep -Pom1 'Autoupdate \K\d*') + 1 ))"
 git pull
