38bafccbc002471b70ae632600f0f2ec24dafdf9
Weathercold <weathercold.scr@gmail.com>
2022-07-14 18:13:06 -0400

    Autoupdate 37

--- scripts/dotsync.sh
+++ scripts/dotsync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $(dirname $(readlink -f "$0"))
-./dotdrop.sh update -bfp user
+./dotdrop.sh update -fp user
 ./dotdrop.sh update -bfp system
 ./dotdrop.sh update -bfp catppuccin
 
