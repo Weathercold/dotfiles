d6c7b75da883c792be36bc48fbfba78daa54eb31
Weathercold <weathercold.scr@gmail.com>
2022-07-14 18:05:34 -0400

    Autoupdate 36

--- scripts/dotsync.sh
+++ scripts/dotsync.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-cd $(dirname $(dirname $(readlink -f "$0")))
+cd $(dirname $(readlink -f "$0"))
 ./dotdrop.sh update -bfp user
 ./dotdrop.sh update -bfp system
 ./dotdrop.sh update -bfp catppuccin
+
+cd $(dirname $(pwd))
 git add .
 git commit -m "Autoupdate $(( $(git log -10 --pretty=format:%B | grep -Pom1 'Autoupdate \K\d*') + 1 ))"
 git pull
